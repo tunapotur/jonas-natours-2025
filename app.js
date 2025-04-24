113319e94b7cd10409319334a84867a15edad37d
tunapotur <tunapotur@yahoo.com>
2025-04-24 14:20:15 +0300

    Creating Our Own Middleware

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const express = require('express');
 
 const app = express();
 
-//middleware
+//middlewares
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋🏻');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
 };
 
 const getTour = (req, res) => {
