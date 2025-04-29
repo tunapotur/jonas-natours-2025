6351b0ccbd6344046a318a8197fdcf2d70a07373
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-04-29 20:32:55 +0300

    MongoDB with Mongoose: Making the API Better: Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,6 +14,7 @@ exports.getAllTours = async (req, res) => {
     */
 
     //BUILD QUERY
+    // 1) Filtering
     /* 
     * Obje kopyalama
     ... operatörü ile obje açılıyor 
@@ -25,9 +26,18 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    console.log(req.query);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    //{ difficulty: 'easy', duration: { $gte: '5' } }
+    //{ difficulty: 'easy', duration: { gte: '5' } }
+    // gte, gt, lte, lt
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
