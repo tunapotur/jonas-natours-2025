a498012c3e58aec061d2641e34c0af4afdb999e6
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-05-03 12:53:02 +0300

    MongoDB with Mongoose: Making the API Better: Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,7 +23,7 @@ exports.getAllTours = async (req, res) => {
     böylelikle objenin yeni bir kopyası oluşuyor 
     */
     const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    const excludedFields = ['sort', 'fields', 'page', 'limit'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
@@ -50,6 +50,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    //page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
     }
 
     // EXECUTE QUERY
--- notes/02 MongoDB/notes.md
+++ notes/02 MongoDB/notes.md
@@ -9,3 +9,9 @@ Sort by price Descendent and ratingsAverage http://localhost:3000/api/v1/tours?s
 Field Limting http://localhost:3000/api/v1/tours?fields=name,duration,difficulty,price,ratingsAverage
 
 http://localhost:3000/api/v1/tours?fields=name,duration,difficulty,price,ratingsAverage&sort=-price,ratingsAverage
+
+    4 Pagination
+    page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page3
+    query = query.skip(10).limit(10);
+
+http://localhost:3000/api/v1/tours?page=2&limit=10
