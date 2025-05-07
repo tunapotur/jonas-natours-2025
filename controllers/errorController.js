632d95d2ad7779d3851dd15d4a080dacbd30a7f7
tunapotur <tunapotur@yahoo.com>
2025-05-07 14:48:27 +0300

    Error Handling with Express: Errors During Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    //1) Log error
+    /** profesyonel bir yazılımda production modunda meydana gelen hatalar
+     * bir LOG dosyasına yazılır. Bu uygulamada böyle bir düzeneğe gerek duyulmuyor
+     * Ayrıca; Heroku gibi platformlarda ürünümüz yayınlandığın alttaki console.error()
+     * fonksiyonun mesajları otomatik olarak LOG dosyasına yazılabiliyormuş.
+     */
+    console.error('ERROR 💥', err);
+
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   /** yakalanan hata mesajının StackTrace bilgisi
    * bu hatanın kod üzerinde nerelerde oluştuğunun
@@ -9,8 +44,9 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   /** res.status() metodu ile hata kullanıcıya döndürülüyor */
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
