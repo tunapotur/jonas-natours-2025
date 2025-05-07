56f256353b30d2a1c6507a2e909781388ce50d8d
tunapotur <tunapotur@yahoo.com>
2025-05-07 14:07:37 +0300

    Error Handling with Express: Adding 404 Not Found Errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -31,6 +32,18 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({ _id: req.params.id })
 
+  /**
+   * aranan id yoksa tour değeri null döner.
+   * Eğer tour değeri null ise next() fonksiyonuyla
+   * globalErrorHandler()' fonksiyonuna hata gönderilir.
+   * return kullanılmasının sebebi; hata gönderilmesi ile birlikte
+   * fonksiyondan çıkılması gerekliliğidir. Aksi takdirde
+   * res.status() adımına geçilir ve kullanıcıya 2 mesaj döndürülür
+   */
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -56,6 +69,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -65,7 +82,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
