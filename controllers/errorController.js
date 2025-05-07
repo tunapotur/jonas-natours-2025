446965f1d19c61d2fe1a4821a9f93287d18dc7cf
tunapotur <tunapotur@yahoo.com>
2025-05-07 16:10:17 +0300

    Error Handling with Express: Handling Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  console.log('******** Handle Cast Error DB ********');
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -42,11 +50,14 @@ module.exports = (err, req, res, next) => {
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
   /** res.status() metodu ile hata kullanıcıya döndürülüyor */
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
