e30d6600173d19d7fb0592f0ca04710d7e2ff244
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-05-08 11:27:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
