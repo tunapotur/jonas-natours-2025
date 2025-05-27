39c75f49508b055f978e880575953bfc12a93252
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-05-27 21:51:22 +0300

    findAndUpdate and Delete Jonas versiyon

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -66,12 +66,19 @@ reviewSchema.statics.calcAverageRating = async function (tourId) {
       },
     },
   ]);
-  console.log(stats);
+  // console.log(stats);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 reviewSchema.post('save', function () {
@@ -79,6 +86,29 @@ reviewSchema.post('save', function () {
   this.constructor.calcAverageRating(this.tour);
 });
 
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRating(this.r.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
+
+/**
+ * 
+reviewSchema.post(/^findOneAnd/, async function(doc) {
+  if (doc) {
+    await doc.constructor.calcAverageRating(doc.tour);
+  }
+});
+
+ */
